cda1073ec45d99da957901764d3947c1aa6b0385
Ndzfh Amrh <nadzifahsamsudin@gmail.com>
2024-01-03 11:33:58 +0800

    update water java

--- EcoFootprintProject/src/main/java/my/ip/ecofootprint/model/Water.java
+++ EcoFootprintProject/src/main/java/my/ip/ecofootprint/model/Water.java
@@ -2,4 +2,13 @@ package my.ip.ecofootprint.model;
 
 public class Water {
 
+	private int waterNumOfDays;
+	private double waterUsageRM;
+	private double waterUsageM3;
+	private String waterMethods;
+	private String waterPractices;
+	private String waterMonth;
+	private long waterPropFactor;
+	private byte[] waterBill;
+	
 }
